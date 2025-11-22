c7a5fd4a9af30f6c4d918823007abb977f339327
srushti32 <hsrushti320@gmail.com>
2025-11-22 12:58:47 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,12 +1,16 @@
 import sys
-#check if correct number of argu
-if len (sys.argv)! = 3:
-print("uasge: python student.py <name><rollno>")
-#sys.argv[0] is always the program name
+
+# check if correct number of arguments
+if len(sys.argv) != 3:
+    print("Usage: python student.py <name> <rollno>")
+    sys.exit(1)  # exit if arguments are not correct
+
+# sys.argv[0] is always the program name
 script_name = sys.argv[0]
 name = sys.argv[1]
 rollno = sys.argv[2]
 
-print("script name:",script_name)
-print("student name:",name)
-print("roll number:",rollno)
+print("Script name:", script_name)
+print("Student name:", name)
+print("Roll number:", rollno)
+
